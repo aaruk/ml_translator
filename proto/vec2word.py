839a8ded1f85c4e413c73d8d38ce51e733daf0eb
aaru <arumugam.kalaikannan@gmail.com>
2017-03-19 17:19:27 -0400

    Debugged & tested first implementation of decoder (vec2word)

--- proto/vec2word.py
+++ proto/vec2word.py
@@ -4,7 +4,7 @@
 import numpy as np
 
 
-def vec2word(in_vec=None, word2veclist=None, out_word=""):
+def vec2word(in_vec=None, vocab=None, word2vec_arr=None, out_word=""):
   """
   Function takes a k-D vector(point) and finds closest match from a point
   cluster
@@ -16,13 +16,24 @@ def vec2word(in_vec=None, word2veclist=None, out_word=""):
   word2vec_list : list of vectors representing all words in vocabulary
   out_word      : word corresponding to input vector
   """
-  dist = abs(in_vec - word2veclist) # Find Euclidean distance between input
+  dist = abs(in_vec - word2vec_arr) # Find Euclidean distance between input
   dist = dist * dist                # vector and all vectors in dictionary
+  dist = dist.sum(axis=1)
+  dist = np.sqrt(dist)
   word_idx = np.argmin(dist)
 
-  # If distance is within acceptable limits, 
+  # If distance is within acceptable limits, pull word from vocabulary
   if dist[word_idx] < 0.1:
-    word = word_list[word_idx]
+    word = vocab[word_idx]
   else:
     word = "UNK"
   return word
+
+if __name__ == "__main__":
+  word_vec =  np.load("data/word_vec.npy")
+  vocab  = np.load("data/vocab.npy")
+  word_list = []
+  for i in np.arange(word_vec.shape[0]):
+    word_list.append(vec2word(word_vec[i], vocab, word_vec))
+  print vocab
+  print word_list
